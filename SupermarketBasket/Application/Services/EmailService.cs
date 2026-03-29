476d40f51da5dd311c25dce51ce82ce308e13467
lukasimonishvili <37000830+lukasimonishvili@users.noreply.github.com>
2026-03-30 01:38:05 +0200

    smtp update

--- SupermarketBasket/Application/Services/EmailService.cs
+++ SupermarketBasket/Application/Services/EmailService.cs
@@ -8,23 +8,33 @@ public class EmailService : IEmailService
 {
     public async Task SendEmailAsync(string to, string subject, string body)
     {
-        var smtpClient = new SmtpClient("smtp.gmail.com")
+        try
         {
-            Port = 587,
-            Credentials = new NetworkCredential("lukasimonishvili1998@gmail.com", "tzvj nxvs rnhw cnnl"),
-            EnableSsl = true,
-        };
+            var emailUser = Environment.GetEnvironmentVariable("lukasimonishvili1998@gmail.com");
+            var emailPass = Environment.GetEnvironmentVariable("tzvj nxvs rnhw cnnl");
 
-        var mail = new MailMessage
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(emailUser, emailPass),
+                EnableSsl = true,
+            };
+
+            var mail = new MailMessage
+            {
+                From = new MailAddress(emailUser!),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            };
+
+            mail.To.Add(to);
+
+            await smtpClient.SendMailAsync(mail);
+        }
+        catch (Exception ex)
         {
-            From = new MailAddress("lukasimonishvili1998@gmail.com"),
-            Subject = subject,
-            Body = body,
-            IsBodyHtml = true
-        };
-
-        mail.To.Add(to);
-
-        await smtpClient.SendMailAsync(mail);
+            Console.WriteLine("EMAIL FAILED: " + ex.Message);
+        }
     }
 }
